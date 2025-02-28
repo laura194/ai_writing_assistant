c34314fb73fdc2e4be89e9322cc0e2c2fcf58569
laura194 <voss.2000@gmx.de>
2025-02-28 10:43:16 +0100

    first commit

---
+++ src/components/FileContentCard.tsx
@@ -0,0 +1,48 @@
+import ReactMarkdown from "react-markdown";
+import { Node } from "../utils/types";
+import { getIcon } from "../utils/icons";
+
+/**
+ * Properties for the FileContentCard component.
+ * @interface FileContentCardProps
+ * @property {Node} node - The node representing the file to display.
+ */
+export interface FileContentCardProps {
+  node: Node;
+}
+
+/**
+ * FileContentCard component that renders a card displaying file content with an icon, title, and markdown content.
+ * @component
+ * @param {FileContentCardProps} props - The properties passed to the component.
+ * @returns- The rendered file content card component.
+ */
+function FileContentCard({ node }: FileContentCardProps) {
+  return (
+    <div className="relative p-4 shadow-lg rounded-lg bg-gray-200">
+      {/* Icon oben rechts */}
+      <div className="absolute top-3 right-3 ">{getIcon(node, "size-8")}</div>
+      <h2 className="text-lg font-bold mb-4">{node.name}</h2>
+      {/* Markdown ohne className, stattdessen über `components` gestylt */}
+      <ReactMarkdown
+        components={{
+          p: ({ children }) => <p className="mt-2">{children}</p>,
+          strong: ({ children }) => (
+            <strong className="font-bold">{children}</strong>
+          ),
+          ul: ({ children }) => (
+            <ul className="list-disc pl-5 mt-2">{children}</ul>
+          ),
+          ol: ({ children }) => (
+            <ol className="list-decimal pl-5 mt-2">{children}</ol>
+          ),
+          li: ({ children }) => <li className="mt-1">{children}</li>,
+        }}
+      >
+        {node.content}
+      </ReactMarkdown>
+    </div>
+  );
+}
+
+export default FileContentCard;
---
+++ src/components/Folder.tsx
@@ -0,0 +1,81 @@
+import { ChevronRightIcon, FolderIcon } from "@heroicons/react/24/solid";
+import File from "./File";
+import { useState, useEffect } from "react";
+import { Node } from "../utils/types";
+
+/**
+ * FolderProps interface that defines the expected properties for the Folder component.
+ * @interface FolderProps
+ * @property {Node} node - The folder node data, which includes information such as the folder's name, ID, and any nested child nodes.
+ * @property {Function} onNodeClick - A callback function that is triggered when a file or folder is clicked. This function accepts a Node object as an argument.
+ */
+interface FolderProps {
+  node: Node;
+  onNodeClick: (node: Node) => void;
+}
+
+/**
+ *
+ * Folder component that represents a folder in a hierarchical file structure.
+ * This component renders the folder's name, its contents (if any), and handles the opening and closing of the folder.
+ * It manages the state of whether the folder is open or closed, saving this state in localStorage for persistence.
+ * It renders nested folders recursively and allows interaction through clicks to toggle folder visibility.
+ * @component
+ * @param {FolderProps} props - The properties passed to the Folder component.
+ * @param {Node} props.node - The folder's data, including its name, ID, and potential child nodes.
+ * @param {Function} props.onNodeClick - A callback function to handle the click event for a file or folder.
+ * @returns - A list item representing the folder, with possible child folders rendered below.
+ */
+function Folder({ node, onNodeClick }: FolderProps) {
+  if (!node) return null;
+
+  const storageKey = `isOpen-${node.id}`;
+  const [isOpen, setIsOpen] = useState<boolean>(() => {
+    const savedState = localStorage.getItem(storageKey);
+    return savedState ? JSON.parse(savedState) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(isOpen));
+  }, [isOpen, storageKey]);
+
+  return (
+    <li className="my-1.5">
+      <span className="flex items-center gap-1.5">
+        {node.nodes && node.nodes.length > 0 && (
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsOpen(!isOpen);
+            }}
+          >
+            <ChevronRightIcon
+              className={`size-4 text-gray-500 ${isOpen ? "rotate-90" : ""}`}
+            />
+          </button>
+        )}
+        {node.nodes ? (
+          <>
+            <FolderIcon className="size-6 text-gray-700" />
+            {node.name}
+          </>
+        ) : (
+          <File node={node} onClick={() => onNodeClick(node)} />
+        )}
+      </span>
+      {isOpen && node.nodes && (
+        <ul className="pl-6">
+          {node.nodes.map((childNode) => (
+            <Folder
+              node={childNode}
+              key={childNode.id}
+              onNodeClick={onNodeClick}
+            />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+}
+
+export default Folder;
