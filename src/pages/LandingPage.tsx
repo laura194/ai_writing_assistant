0ff084a2682c30581b43b8f1bd6c80ff9af735e1
lisakutemeier <abced1@web.de>
2025-05-07 14:04:56 +0200

    changed routing name from app to editPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
         <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
             <h1 className="text-3xl font-bold mb-8">Willkommen!</h1>
             <button
-                onClick={() => navigate("/app")}
+                onClick={() => navigate("/editPage")}
                 className="bg-blue-500 text-white py-2 px-4 rounded mb-4 hover:bg-blue-600"
             >
                 Bestehendes Projekt öffnen
