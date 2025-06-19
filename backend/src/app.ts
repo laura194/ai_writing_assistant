edfa10fbdbc8e26de1f994ebdbaa7526b44ff5f8
laura194 <voss.2000@gmx.de>
2025-06-19 15:01:23 +0200

    added nodeContentRoutes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import helloRoutes from "./routes/hello.routes";
 import connectDB from "./db";
 import aiRoutes from "./routes/ai.routes";
+import nodeContentRoutes from "./routes/nodeContent.routes";
 
 const app = express();
 
@@ -15,5 +16,6 @@ connectDB();
 // Routen
 app.use("/api/hello", helloRoutes);
 app.use("/api/ai", aiRoutes);
+app.use("/api/nodeContent", nodeContentRoutes);
 
 export default app;
