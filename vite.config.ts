5ff2e67907ae0fa91c5c65dedb369ba1593d05a0
laura194 <voss.2000@gmx.de>
2025-09-03 18:35:03 +0200

    eee

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
+const API_BASE_URL = process.env.VITE_API_BASE_URL || "http://localhost:5001";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
   server: {
     proxy: {
-      "/api": "http://localhost:5001",
+      "/api": API_BASE_URL,
     },
   },
 });
