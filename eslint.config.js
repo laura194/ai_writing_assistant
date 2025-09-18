6f820c2b4fa14716767b816af8142a948cc98a79
Gero Stoewe <gerostoewe@gmx.de>
2025-09-18 11:59:06 +0200

    #69:
    PPPLLLLEEEEEAAAAAASSSSSEEEEE!!!

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import ts from "@typescript-eslint/eslint-plugin";
 
 export default [
-  js.configs.recommended,
-  ts.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     ignores: [
@@ -30,6 +27,8 @@ export default [
       "react-refresh": reactRefresh,
     },
     rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "warn",
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
@@ -37,4 +36,5 @@ export default [
       ],
     },
   },
+  js.configs.recommended,
 ];
