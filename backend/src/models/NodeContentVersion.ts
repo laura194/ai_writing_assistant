96e73593c8ce9e4fc8ddc3b9a2699edc1a11768e
Eileen-34 <oymak.eileen@gmail.com>
2025-12-20 00:15:37 +0100

    add encryption hook in node content version ts

--- backend/src/models/NodeContentVersion.ts
+++ backend/src/models/NodeContentVersion.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
-
+import { encryptValue, decryptValue, isEncryptionEnabled } from "../utils/encryption";
 export interface INodeContentVersion extends Document {
   nodeId: string;
   projectId: string;
@@ -25,6 +25,61 @@ const NodeContentVersionSchema: Schema = new Schema(
 );
 
 NodeContentVersionSchema.index({ nodeId: 1, projectId: 1, createdAt: -1 });
+
+// Encryption hook - before saving
+NodeContentVersionSchema.pre("save", function (next) {
+  if (!isEncryptionEnabled()) {
+    return next();
+  }
+
+  try {
+    // Encrypt content
+    if (this.isModified("content") && this.content) {
+      this.content = encryptValue(this.content as string);
+    }
+
+    // Encrypt name
+    if (this.isModified("name") && this.name) {
+      this.name = encryptValue(this.name as string);
+    }
+
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
+});
+
+// Decryption hook - after finding multiple documents
+NodeContentVersionSchema.post("find", function (docs: INodeContentVersion[]) {
+  if (!isEncryptionEnabled() || !docs) {
+    return;
+  }
+
+  docs.forEach((doc) => {
+    if (doc.content) {
+      doc.content = decryptValue(doc.content);
+    }
+
+    if (doc.name) {
+      doc.name = decryptValue(doc.name);
+    }
+  });
+});
+
+// Decryption hook - after finding one document
+NodeContentVersionSchema.post("findOne", function (doc: INodeContentVersion | null) {
+  if (!isEncryptionEnabled() || !doc) {
+    return;
+  }
+
+  if (doc.content) {
+    doc.content = decryptValue(doc.content);
+  }
+
+  if (doc.name) {
+    doc.name = decryptValue(doc.name);
+  }
+});
 
 export default mongoose.models.NodeContentVersion ||
   mongoose.model<INodeContentVersion>(
